303827c059806a084ba2b057902bf83034c49ccf
Junyong Park <jyp0802@hotmail.com>
2021-09-14 23:48:16 +0900

    Make graph nodes ordered

--- graph_construct/data.py
+++ graph_construct/data.py
@@ -1,56 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
 high_level_nodes = ["cut_and_mix_ingredients", "prepare_dressing", "serve_salad"]
-
-#f = open("C:/Users/wkdgy/Desktop/3학년 여름 개별연구/01-2-activityAnnotation.txt",'r')
-f = open("C:/Users/wkdgy/Desktop/ann-ts/activityAnnotations/02-1-activityAnnotation.txt", 'r')
-
-data = []
-action_list = []
-
-while True:
-
-    line = f.readline()    
-    if not line: break
-    
-    tokens = line.split(' ')
-    if tokens[2][:-1] in high_level_nodes:
-        continue
-    action_list.append([tokens[0], tokens[2][:-1]])
-
-action_list = sorted(action_list, key=lambda x : int(x[0]))
-
-refined_action = []
-for action in action_list:
-    label = action[1]
-    type = label.split('_')[-1]
-    if type in ['core', 'prep', 'post']:
-        label = "_".join(label.split('_')[:-1])
-    if len(refined_action) == 0 or refined_action[-1] != label:
-        refined_action.append(label) 
-
-print(refined_action)
-data = refined_action
-    # print(line)
-    # line = line
-
-    # if line[-4:] == 'core' or 'prep' or 'post':
-    #     line = line[:-5]
-    
-    # print(line)
-    # data.append(line)
-
-#print(data)
-f.close()
-
-"""
-prep : 준비, 처음에 돌릴 때
-
-"""
-
-DG = nx.DiGraph()
-node = ["cut_and_mix_ingredients","peel_cucumber_prep","peel_cucumber_core","peel_cucumber_post",
+all_nodes = ["cut_and_mix_ingredients","peel_cucumber_prep","peel_cucumber_core","peel_cucumber_post",
 "cut_cucumber_prep", "cut_cucumber_core", "cut_cucumber_post", "place_cucumber_into_bowl_prep", "place_cucumber_into_bowl_core", "place_cucumber_into_bowl_post",
 "cut_tomato_prep", "cut_tomato_core", "cut_tomato_post", "place_tomato_into_bowl_prep", "place_tomato_into_bowl_core", "place_tomato_into_bowl_post",
 "cut_cheese_prep", "cut_cheese_core", "cut_cheese_post", "place_cheese_into_bowl_prep", "place_cheese_into_bowl_core", "place_cheese_into_bowl_post",
@@ -59,20 +12,82 @@ node = ["cut_and_mix_ingredients","peel_cucumber_prep","peel_cucumber_core","pee
 "add_salt_prep", "add_salt_core", "add_salt_post", "add_pepper_prep", "add_pepper_core", "add_pepper_post", "mix_dressing_prep", "mix_dressing_core", "mix_dressing_post",
 "serve_salad_onto_plate_prep", "serve_salad_onto_plate_core", "serve_salad_onto_plate_post", "add_dressing_prep", "add_dressing_core", "add_dressing_post"]
 
-# for i in node:
-#     DG.add_node(i)
+def get_node_list(annotation_file):
+    f = open(annotation_file, 'r')
+    lines = f.readlines()
+    f.close()
 
-for idx, i in enumerate(data):
-    DG.add_node(idx, name=i)
+    all_actions = []
 
-for i in range(len(data)-1):
-    # DG.add_edge(data[i], data[i+1])
-    DG.add_edge(i, i+1)
+    for line in lines:
+        tokens = line.split(' ')
+        start_time = tokens[0]
+        action_name = tokens[2][:-1]
+        if action_name in high_level_nodes:
+            continue
+        all_actions.append([start_time, action_name])
 
-label_dict = {}
-for idx, i in enumerate(data):
-    label_dict[idx] = i
+    all_actions = sorted(all_actions, key=lambda x : int(x[0]))
 
-nx.draw(DG, labels = label_dict, with_labels = True)
-# nx.draw_networkx_labels(DG, data)
+    refined_actions = []
+
+    for _, action_name in all_actions:
+        action_split = action_name.split('_')
+        if action_split[-1] in ['core', 'prep', 'post']:
+            action_name = "_".join(action_split[:-1])
+        if len(refined_actions) == 0 or refined_actions[-1] != action_name:
+            refined_actions.append(action_name) 
+
+    return refined_actions
+
+def get_node_pos(i, node_count):
+    x = i % GRAPH_WIDTH
+    y = i // GRAPH_WIDTH + node_count // GRAPH_WIDTH
+    if (i // GRAPH_WIDTH) % 2 == 1:
+        x = GRAPH_WIDTH - x - 1
+    return (x, -y*2)
+
+def draw_graph(idx, plot_file, node_list):
+    global node_count
+
+    for i, name in enumerate(node_list):
+        if i == 0:
+            name = "{}\n\n{}\n\n".format(plot_file, name)
+        DG.add_node("{}_{}".format(idx, i), name=name, pos=get_node_pos(i, node_count))
+
+    for i in range(len(node_list)-1):
+        DG.add_edge("{}_{}".format(idx, i), "{}_{}".format(idx, i+1))
+
+    node_count += len(node_list)
+    node_count = GRAPH_WIDTH * math.ceil(node_count/GRAPH_WIDTH)
+
+'''
+Hyper-parameters
+'''
+LABEL_ROTATION = 30
+GRAPH_WIDTH = 15
+FONT_SIZE = 8
+plot_list = ["01-1", "02-1", "03-1"]
+
+
+f = plt.figure(figsize=(GRAPH_WIDTH, int(GRAPH_WIDTH/5*len(plot_list))))
+r = f.canvas.get_renderer()
+plt.axis('equal')
+
+DG = nx.DiGraph()
+node_count = 0
+
+for idx, plot_file in enumerate(plot_list):
+    node_list = get_node_list("../activityAnnotations/{}-activityAnnotation.txt".format(plot_file))
+    draw_graph(idx, plot_file, node_list)
+
+labels = nx.get_node_attributes(DG, 'name') 
+pos = nx.get_node_attributes(DG, 'pos') 
+nx.draw(DG, pos=pos)
+description = nx.draw_networkx_labels(DG, pos=pos, labels=labels, font_size=FONT_SIZE)
+for node, t in description.items():
+    t.set_rotation(LABEL_ROTATION)
+
+print(DG.nodes(data=True))
+
 plt.show()
